39ce6ae30d3069407482db657eb62ae86b4255fc
unknown <zakir_hockey@mail.ru>
2019-06-13 20:35:41 +0300

    You

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,1 @@
-var button = document.querySelector ('#button');
-var modal = document.querySelector ('#modal');
-var close = document.querySelector ('#close');
-
-button.addEventListener ('click', function(){
-    modal.classList.add ('modal_active');
-})
-
-close.addEventListener ('click', function(){
-    modal.classList.remove ('modal_active');
-})
+var button = document.querySelector ('#button');var modal = document.querySelector ('#modal');var close = document.querySelector ('#close');button.addEventListener ('click', function(){modal.classList.add ('modal_active');})close.addEventListener ('click', function(){modal.classList.remove ('modal_active');})
